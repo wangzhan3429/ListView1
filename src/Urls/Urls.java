a7638d529744dfceeebca6d579fb0800d35bd98a
wangzhan3429 <13124757783@163.com>
2016-09-17 19:38:39 +0800

    新建项目

---
+++ src/Urls/Urls.java
@@ -0,0 +1,9 @@
+package Urls;
+
+public class Urls {
+	
+	public static String SplishImgUrl = "http://news-at.zhihu.com/api/4/start-image/1080*1776";
+	
+	
+
+}
---
+++ src/com/wz/thelast/MainActivity.java
@@ -0,0 +1,59 @@
+package com.wz.thelast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	private LinearLayout invis;
+    private ListView lv;
+    String[] strs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        invis = (LinearLayout) findViewById(R.id.invis);
+
+        strs = new String[100];
+
+        for (int i = 0; i < 100; i++) {
+            strs[i] = "data-----" + i;
+        }
+
+        lv = (ListView) findViewById(R.id.lv);
+        View header1 = View.inflate(this, R.layout.first, null);//头部内容
+        lv.addHeaderView(header1);//添加头部
+
+        View header2 = View.inflate(this, R.layout.second, null);
+        lv.addHeaderView(header2);
+
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, strs));
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem >= 1) {
+                    invis.setVisibility(View.VISIBLE);
+                } else {
+                    invis.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+	
+}
